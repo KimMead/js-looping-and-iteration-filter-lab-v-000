6c6cf22e9f507393dbd1d22127bf5ae6acb19822
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2019-06-14 22:40:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Code your solution in this file
+function findMatching (drivers, string){
+  return drivers.filter(function (driver){
+    return driver.toLowerCase() === string.toLowerCase();
+    
+  })
+}
